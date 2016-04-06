9b3740a13c9b9801fe9d5006f866927825557ffc
JiaohuangSGF <1041884776@qq.com>
2016-04-06 15:55:25 +0800

    饮料类

---
+++ src/Decorator/CondimentDecorator.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+/**
+ * 
+ * ClassName: CondimentDecorator 
+ * @Description: 调料抽象类
+ * @author Mr.Song
+ * @date 2016年4月6日  下午3:50:48
+ */
+public abstract class CondimentDecorator extends Beverage{
+
+	public abstract String getDescription();
+}
